8b765bc85dc0ad2f610c77b5ddeb5c2a6ccf12dc
Konrad Ziarko <konrad19931118@gmail.com>
2016-11-29 22:32:10 +0100

    Added some animation.

--- SudokuSolver/SudokuSolver.py
+++ SudokuSolver/SudokuSolver.py
@@ -1,8 +1,41 @@
 import numpy as np
+import os
 import time
 from colorama import init, Fore, Back, Style
-init(autoreset=True)
+import threading
+from msvcrt import getch
+init(autoreset=False)
+from ctypes import *
+ 
+STD_OUTPUT_HANDLE = -11
+ 
+class COORD(Structure):
+    pass
+ 
+COORD._fields_ = [("X", c_short), ("Y", c_short)]
 
+def printColoredSudoku(before, testData):
+    for i in range (0,9):
+        for j in range (0,9):
+            if before[i][j] == testData[i][j] and before[i][j] != 0:
+                print(Fore.GREEN + str(testData[i][j]), end="")
+            elif testData[i][j] == 0:
+                print(Fore.RED + str(testData[i][j]), end="")
+            else:
+                print(Fore.LIGHTCYAN_EX + str(testData[i][j]), end="")
+            
+            if (j+1) % 3 == 0:
+                print("|", end="")
+        if (i+1) % 3 == 0:
+                print()
+                print("--"*6, end="")
+        print ("")
+def print_at(r, c, s):
+    h = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+    windll.kernel32.SetConsoleCursorPosition(h, COORD(c, r))
+ 
+    c = s.encode("windows-1252")
+    windll.kernel32.WriteConsoleA(h, c_char_p(c), len(c), None, None)
 class SingleCell:
     def __init__(self, x, y, houseNumber, row, col, house, missingInRow, missingInColumn, missingInHouse):
         self.me = 0
@@ -175,6 +208,82 @@ class SudokuGrid:
                 return False
         return True;
 
+    def animatedSolve(self):
+        breakNextLoop = False
+        for loops in range(0,10):
+            if breakNextLoop:
+                break
+            breakNextLoop = True
+            breakLoop = False
+            for i in range(0, 20):
+                if breakLoop:
+                    """print("simple", i)"""
+                    break
+                breakLoop = True
+                for obj in self.cells:
+                    obj.clearLists()
+                    if obj.simpleCheck():
+                        breakLoop = False
+                        self.grid[obj.x, obj.y] = obj.me
+                        if abortable_sleep(1):
+                            pass
+                        print_at(obj.x+obj.houseNumber[0], obj.y+obj.houseNumber[1], str(obj.me))
+                        self.cells.remove(obj)
+
+            breakLoop = False
+            for i in range(0, 20):
+                if breakLoop:
+                    """print("missing", i)"""
+                    break
+                breakLoop = True
+                for obj in self.cells:
+                    obj.clearLists()
+                    if obj.simpleCheck():
+                        self.grid[obj.x, obj.y] = obj.me
+                        if abortable_sleep(1):
+                            pass
+                        print_at(obj.x+obj.houseNumber[0], obj.y+obj.houseNumber[1], str(obj.me))
+                        self.cells.remove(obj)
+                        continue
+                    obj.potencialNumbers = list(set(obj.missingInHouse).intersection(set(obj.missingInColumn).intersection(obj.missingInRow)))
+                    for obj2 in self.cells:
+                        if obj2.houseNumber == obj.houseNumber and obj2!=obj:
+                            """obj.potencialNumbers = list(set(obj.potencialNumbers).intersection(set(obj.missingInHouse).intersection(set(obj.missingInColumn).intersection(obj.missingInRow))))"""
+                            obj2.potencialNumbers = list(set(obj2.missingInHouse).intersection(set(obj2.missingInColumn).intersection(obj2.missingInRow)))
+                            obj.potencialNumbers = list(set(obj.potencialNumbers).difference(obj2.potencialNumbers))
+                            if len(obj.potencialNumbers) == 0:
+                                        break
+                    if len(obj.potencialNumbers)==1:
+                        obj.potencialNumbers = list(set(obj.potencialNumbers).intersection(set(obj.missingInHouse).intersection(set(obj.missingInColumn).intersection(obj.missingInRow))))
+                        if len(obj.potencialNumbers)==1:
+                            breakLoop = False
+                            self.grid[obj.x, obj.y] = obj.me = obj.potencialNumbers[0]
+                            if abortable_sleep(1):
+                                pass
+                            print_at(obj.x+obj.houseNumber[0], obj.y+obj.houseNumber[1], str(obj.me))
+                            self.cells.remove(obj)
+            
+            
+            for i in range (0,9):
+                for j in range (0,9):
+                    if self.grid[i,j] == 0:
+                        breakNextLoop=False;
+                        break
+                if not breakNextLoop:
+                    break
+
+        print_at(15,0, "ESC to exit\n")
+        """print("loops", loops)"""
+        for i in range (0,9):
+            test = set(self.grid[i, :])
+            if len(test) != 9:
+                return False
+        for i in range (0,9):
+            test = set(self.grid[:, i])
+            if len(test) != 9:
+                return False
+        return True;
+
 testData5 = np.array([[1,4,0,2,0,8,5,0,7],[9,0,8,0,0,0,0,0,0],[0,5,6,1,0,3,9,2,8],[3,6,1,0,5,4,2,8,0],[8,7,0,0,2,0,0,5,0],[5,9,2,0,0,0,0,0,0],[0,3,0,9,0,2,7,0,4],[2,8,0,0,3,1,6,9,0],[4,1,0,0,6,7,8,0,2]])
 
 testData4 = np.array([[0,0,0,5,0,0,0,0,0],[7,5,4,3,6,1,2,0,0],[3,0,0,0,0,0,0,7,6],[8,0,9,4,1,0,6,0,3],[6,3,0,7,0,9,1,0,0],[0,4,0,0,8,0,9,5,0],[5,1,0,9,0,8,0,0,2],[0,0,0,0,0,0,0,0,0],[2,9,0,0,4,7,8,6,0]])
@@ -187,34 +296,56 @@ testData = np.array([[9,0,0,0,0,0,0,0,1],[0,5,0,0,7,0,6,0,0],[1,0,0,0,0,0,7,0,5]
 
 """for i in range (0,9):
     print(len(testData[i]))"""
+
+
 for i in range (0,9):
     for j in range (0,9):
+        if testData[i][j] == 0:
+            print(Fore.WHITE, end="")
+        else:
+            print(Fore.GREEN, end="")
         print(testData[i][j], end="")
         if (j+1) % 3 == 0:
+            print(Fore.MAGENTA, end="")
             print("|", end="")
     if (i+1) % 3 == 0:
             print()
+            print(Fore.MAGENTA, end="")
             print("--"*6, end="")
     print ("")
+
+print(Fore.LIGHTCYAN_EX)
 before = np.array(testData)
 
+
 obj = SudokuGrid(testData)
+class AbortableSleep():
+    def __init__(self):
+        self._condition = threading.Condition()
+
+    def __call__(self, secs):
+        with self._condition:
+            self._aborted = False
+            self._condition.wait(timeout=secs)
+            return not self._aborted
+
+    def abort(self):
+        with self._condition:
+            self._condition.notify()
+            self._aborted = True
+abortable_sleep = AbortableSleep()
+threading.Thread(target=obj.animatedSolve).start()
+
+while True:
+    key = ord(getch())
+    abortable_sleep.abort()
+    if key == 27: #ESC
+        break
+
+"""
 start_time = time.time()
 print(obj.trySolve())
 print(time.time() - start_time, "\n")
+"""
 
-for i in range (0,9):
-    for j in range (0,9):
-        if before[i][j] == testData[i][j] and before[i][j] != 0:
-            print(Fore.GREEN + str(testData[i][j]), end="")
-        elif testData[i][j] == 0:
-            print(Fore.RED + str(testData[i][j]), end="")
-        else:
-            print(Fore.LIGHTCYAN_EX + str(testData[i][j]), end="")
-            
-        if (j+1) % 3 == 0:
-            print("|", end="")
-    if (i+1) % 3 == 0:
-            print()
-            print("--"*6, end="")
-    print ("")
+
